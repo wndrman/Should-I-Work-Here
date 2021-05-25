f9dedf84d095c526150ef323580aacb4669e7e25
Simon <simon.kidame45@gmail.com>
2021-05-25 23:16:25 +0200

    delete all

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Footer } from './Footer/Footer';
-export { default as ReviewSection } from './ReviewSection/ReviewSection';
-export { default as InfoSection } from './InfoSection/InfoSection';
-export { default as AboutSection } from './AboutSection/AboutSection';
